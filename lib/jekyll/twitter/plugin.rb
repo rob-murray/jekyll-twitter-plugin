d5e6e67a928383be620d609ffa63cc8c64f577f9
Rob Murray <robmurray17@gmail.com>
2014-10-05 12:51:50 +0100

    Gem skeleton

---
+++ lib/jekyll/twitter/plugin.rb
@@ -0,0 +1,9 @@
+require "jekyll/twitter/plugin/version"
+
+module Jekyll
+  module Twitter
+    module Plugin
+      # Your code goes here...
+    end
+  end
+end
---
+++ lib/jekyll/twitter/plugin/version.rb
@@ -0,0 +1,7 @@
+module Jekyll
+  module Twitter
+    module Plugin
+      VERSION = "0.0.1"
+    end
+  end
+end
