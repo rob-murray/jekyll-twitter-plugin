80df0d603884474cd355d264a07e7b2d46b88fe6
Rob Murray <robmurray17@gmail.com>
2016-09-18 17:01:53 +0100

    Byebug Byebug Byebug

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -2,7 +2,7 @@
 $LOAD_PATH.unshift File.expand_path("../../lib", __FILE__)
 require "support/jekyll_template"
 require "jekyll-twitter-plugin"
-require "byebug" if defined?("byebug")
+require "byebug" if defined?(Byebug)
 
 #
 # The `.rspec` file also contains a few flags that are not defaults but that
